d8a3d7e7af3fd26effe56fa8b3e03e58da7a0a53
Shravan Murali <shravanmurali@gmail.com>
2018-07-29 21:45:43 +0530

    Implement dunder methods for number operations to duck type proxy objects like integers

--- PyRDF/Proxy.py
+++ PyRDF/Proxy.py
@@ -34,6 +34,184 @@ class Proxy(object):
         """
         self.action_node = action_node
 
+    def __eq__(self, obj):
+        """
+        Checks if the given object is equal to
+        the value of the current action node.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value to which the current action node's
+            value is compared.
+
+        """
+        self._check_and_execute()
+        # Checking both ways is required because there is no reverse
+        # equal dunder like '__req__'.
+        return (self.action_node.value == obj) or (obj == self.action_node.value)
+
+    def __add__(self, obj):
+        """
+        Adds the input object with current
+        action node's value.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value to which the current action node's
+            value is added.
+
+        """
+        self._check_and_execute()
+        return self.action_node.value + obj
+
+    def __radd__(self, obj):
+        """
+        Adds the input object with current
+        action node's value.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value to which the current action node's
+            value is added.
+
+        """
+        self._check_and_execute()
+        return self.action_node.value + obj
+
+    def __sub__(self, obj):
+        """
+        Subtracts the current action node's value
+        from the input object.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            Current action node's value is subtracted
+            from this value.
+
+        """
+        self._check_and_execute()
+        return self.action_node.value - obj
+
+    def __rsub__(self, obj):
+        """
+        Subtracts the the input object from current
+        action node's value.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value from which the current action node's
+            value is subtracted.
+
+        """
+        self._check_and_execute()
+        return obj - self.action_node.value
+
+    def __mul__(self, obj):
+        """
+        Multiplies the input object with current
+        action node's value.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value to which the current action node's
+            value is multiplied.
+
+        """
+        self._check_and_execute()
+        return self.action_node.value * obj
+
+    def __rmul__(self, obj):
+        """
+        Multiplies the input object with current
+        action node's value.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value to which the current action node's
+            value is multiplied.
+
+        """
+        self._check_and_execute()
+        return self.action_node.value * obj
+
+    def __div__(self, obj):
+        """
+        Divides the current action node's value
+        by input object.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value which divides the current
+            action node's value.
+
+        """
+        self._check_and_execute()
+        return self.action_node.value/obj
+
+    def __rdiv__(self, obj):
+        """
+        Divides the input object by current
+        action node's value.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value by which the current action
+            node's value is divided.
+
+        """
+        self._check_and_execute()
+        return obj/self.action_node.value
+
+    def __truediv__(self, obj):
+        """
+        Divides the current action node's value
+        by input object. This method is specifically
+        for division in Python3.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value which divides the current
+            action node's value.
+
+        """
+        self._check_and_execute()
+        return self.action_node.value/obj
+
+    def __rtruediv__(self, obj):
+        """
+        Divides the input object by current
+        action node's value. This method is specifically
+        for division in Python3.
+
+        Parameters
+        ----------
+        obj : numbers.Number or PyRDF.Proxy.Proxy
+            The value by which the current action
+            node's value is divided.
+
+        """
+        self._check_and_execute()
+        return obj/self.action_node.value
+
+    def __str__(self):
+        """
+        Returns the string representation
+        of the current action node's value.
+
+        """
+        self._check_and_execute()
+        return str(self.action_node.value)
+
     def __getattr__(self, attr):
         """
         Intercepts calls on the result of
@@ -50,13 +228,19 @@ class Proxy(object):
         self._cur_attr = attr # Stores the name of operation call
         return self._call_handler
     
-    def _call_handler(self, *args, **kwargs):
-        # Handles an operation call to the current action node
-        # and returns result of the current action node.
+    def _check_and_execute(self):
+        # Checks if execution of computation
+        # got over and triggers the event-loop
+        # if not over.
 
-        if not self.action_node.value: # If event-loop not triggered
+        if not self.action_node.value:
             from . import current_backend
             generator = CallableGenerator(self.action_node.get_head())
             current_backend.execute(generator)
+
+    def _call_handler(self, *args, **kwargs):
+        # Handles an operation call to the current action node
+        # and returns result of the current action node.
+        self._check_and_execute()
 
         return getattr(self.action_node.value, self._cur_attr)(*args, **kwargs)
